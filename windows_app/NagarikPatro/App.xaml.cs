db2251d13c3593df788827ba85c307377088fba4
--show <ashish.thapa477@gmail.com>
2026-03-04 13:10:11 +0545

    fix(windows): qualify Application in App.xaml.cs to resolve WPF/WinForms ambiguity

--- windows_app/NagarikPatro/App.xaml.cs
+++ windows_app/NagarikPatro/App.xaml.cs
@@ -1,9 +1,10 @@
-using System.Windows;
 using NagarikPatro.Views;
 
 namespace NagarikPatro;
 
-public partial class App : Application
+// Qualify explicitly: UseWindowsForms implicit using adds System.Windows.Forms.Application
+// which would otherwise be ambiguous with System.Windows.Application (WPF).
+public partial class App : System.Windows.Application
 {
     private SystemTrayManager? _trayManager;
 
